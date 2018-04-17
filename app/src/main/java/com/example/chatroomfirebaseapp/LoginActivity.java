f58b1de3df94125fd0c2bbbd400b6b9414e98c66
DeepMAC <dghaghara@gmail.com>
2018-04-17 19:35:57 -0400

    Added Signup and Login.

--- app/src/main/java/com/example/chatroomfirebaseapp/LoginActivity.java
+++ app/src/main/java/com/example/chatroomfirebaseapp/LoginActivity.java
@@ -1,13 +1,48 @@
 package com.example.chatroomfirebaseapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private String TAG = "Login";
+    private EditText email, pass;
+    private Button login, signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.editTextEmail);
+        pass = findViewById(R.id.editTextPass);
+        login = findViewById(R.id.buttonLogin);
+        signup = findViewById(R.id.buttonSignUp);
+        mAuth = FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Performing Login");
+                //performLogin(email.getText().toString(), pass.getText().toString());
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Performing Sign Up");
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/chatroomfirebaseapp/SignupActivity.java
+++ app/src/main/java/com/example/chatroomfirebaseapp/SignupActivity.java
@@ -1,13 +1,83 @@
 package com.example.chatroomfirebaseapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private String TAG = "Signup";
+    private EditText fname, lname, email, pass, retypePass;
+    private Button cancel, signup;
+    private String sFName,sLName,sEmail,sPass,sRetype;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        fname = findViewById(R.id.editTextFirstName);
+        lname = findViewById(R.id.editTextLastName);
+        email = findViewById(R.id.editTextEmail2);
+        pass = findViewById(R.id.editTextPass1);
+        retypePass = findViewById(R.id.editTextPass2);
+        cancel = findViewById(R.id.buttonCancel);
+        signup = findViewById(R.id.buttonSignUp2);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                //startActivity(intent);
+                finish();
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: ");
+                sFName = fname.getText().toString();
+                sLName = lname.getText().toString();
+                sEmail = email.getText().toString();
+                sPass = pass.getText().toString();
+                sRetype = retypePass.getText().toString();
+
+                Log.d(TAG, "onClick12: " + sFName.length());
+
+                if(sFName.equals("")) {
+                    Toast.makeText(SignupActivity.this,"Enter First Name!",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(sLName.equals("")) {
+                    Toast.makeText(SignupActivity.this,"Enter Last Name!",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(sEmail.equals("")) {
+                    Toast.makeText(SignupActivity.this,"Enter Email!",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(sPass.equals("")) {
+                    Toast.makeText(SignupActivity.this,"Enter Password!",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(sRetype.equals("")) {
+                    Toast.makeText(SignupActivity.this,"Retype the Password!",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(!sPass.equals(sRetype)){
+                    Toast.makeText(SignupActivity.this,"Passwords Do not Match!",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Log.d(TAG, "Performing Sign Up");
+                //performSignUp(sFName, sLName, sEmail, sPass);
+            }
+        });
+
+
     }
 }
